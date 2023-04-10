c95bd2ab0a244604410e7239713f1d7f7b59b118
aazzizzz1 <azizvbi33@gmail.com>
2023-04-11 01:58:28 +0700

    Preview download file from upload file client

--- lib/widgets/notes/detail_note/client_upload.dart
+++ lib/widgets/notes/detail_note/client_upload.dart
@@ -1,14 +1,111 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:bantuin/constants/color/app_color.dart';
+import 'package:bantuin/constants/font/app_font.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 
-import '../../../constants/color/app_color.dart';
-import '../../../constants/font/app_font.dart';
+class ClientUpload extends StatefulWidget {
+  const ClientUpload({Key? key}) : super(key: key);
 
-class ClientUpload extends StatelessWidget {
-  const ClientUpload({super.key});
+  @override
+  _ClientUploadState createState() => _ClientUploadState();
+}
+
+class _ClientUploadState extends State<ClientUpload> {
+  PlatformFile? _pickedFile;
+
+  void _openFile() {
+    if (_pickedFile != null) {
+      print('File opened: ${_pickedFile!.name}');
+      if (_pickedFile!.extension == 'jpg' || _pickedFile!.extension == 'png') {
+        // Display the image file
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (_) => Scaffold(
+            appBar: AppBar(
+              title: Text(_pickedFile!.name!),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.download_rounded,
+                    color: AppColorNeutral.neutral1,
+                  ),
+                  onPressed: _saveFile,
+                ),
+              ],
+            ),
+            body: Center(
+              child: Image.file(File(_pickedFile!.path!)),
+            ),
+          ),
+        ));
+      } else if (_pickedFile!.extension == 'pdf') {
+        // Display the PDF file
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (_) => Scaffold(
+            appBar: AppBar(
+              title: Text(_pickedFile!.name!),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.download_rounded,
+                    color: AppColorNeutral.neutral1,
+                  ),
+                  onPressed: _saveFile,
+                ),
+              ],
+            ),
+            body: PDFView(
+              filePath: _pickedFile!.path!,
+            ),
+          ),
+        ));
+      } else {
+        print('Unsupported file type');
+      }
+    }
+  }
+
+    void _saveFile() async {
+    if (_pickedFile != null) {
+      print('File saved: ${_pickedFile!.name}');
+      if (_pickedFile!.extension == 'jpg' || _pickedFile!.extension == 'png') {
+        // Save the image file
+        final directory = await getApplicationDocumentsDirectory();
+        final path = directory.path;
+        final file = File(_pickedFile!.path!);
+        final savedFile = await file.copy('$path/${_pickedFile!.name}');
+        print('File saved to: ${savedFile.path}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Image saved to ${savedFile.path}'),
+          ),
+        );
+      } else if (_pickedFile!.extension == 'pdf') {
+        // Save the PDF file
+        final directory = await getApplicationDocumentsDirectory();
+        final path = directory.path;
+        final file = File(_pickedFile!.path!);
+        final savedFile = await file.copy('$path/${_pickedFile!.name}');
+        print('File saved to: ${savedFile.path}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('PDF saved to ${savedFile.path}'),
+          ),
+        );
+      } else {
+        print('Unsupported file type');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Unsupported file type'),
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +118,7 @@ class ClientUpload extends StatelessWidget {
         ),
         const SizedBox(height: 8),
         Text(
-          " File yang support adalah .jpg atau .png.",
+          "File yang support adalah .jpg atau .png.",
           style: AppFont.medium14,
         ),
         const SizedBox(height: 16),
@@ -32,27 +129,24 @@ class ClientUpload extends StatelessWidget {
               allowedExtensions: ['jpg', 'pdf', 'doc'],
             );
             if (result != null) {
-              PlatformFile file = result.files.first;
-              Text(file.name);
-              print(file.name);
-              print(file.bytes);
-              print(file.size);
-              print(file.extension);
-              print(file.path);
+              setState(() {
+                _pickedFile = result.files.first;
+              });
             } else {
               print('Tidak mengambil File');
               // User canceled the picker
             }
           },
-          style: const ButtonStyle(
-            overlayColor: MaterialStatePropertyAll(AppColorNeutral.neutral2),
-            padding: MaterialStatePropertyAll(
-              EdgeInsets.symmetric(vertical: 10),
+          style: ButtonStyle(
+            overlayColor: MaterialStateProperty.all(AppColorNeutral.neutral2),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(vertical: 10),
             ),
-            elevation: MaterialStatePropertyAll(0),
-            side: MaterialStatePropertyAll(
-                BorderSide(color: AppColorNeutral.neutral2)),
-            backgroundColor: MaterialStatePropertyAll(Colors.white),
+            elevation: MaterialStateProperty.all(0),
+            side: MaterialStateProperty.all(
+              const BorderSide(color: AppColorNeutral.neutral2),
+            ),
+            backgroundColor: MaterialStateProperty.all(Colors.white),
           ),
           child: Container(
             alignment: Alignment.center,
@@ -65,6 +159,12 @@ class ClientUpload extends StatelessWidget {
             ),
           ),
         ),
+        const SizedBox(height: 16),
+        if (_pickedFile != null)
+          ElevatedButton(
+            onPressed: _openFile,
+            child: Text('Open file: ${_pickedFile!.name}'),
+          ),
       ],
     );
   }
