ba5d64e3a2d179a07cb90fbcc34bfc489eea4b29
aazzizzz1 <azizvbi33@gmail.com>
2023-04-17 11:15:41 +0700

    floating button for tim and navigation to tim form

---
+++ lib/screens/tim/tim_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimForm extends StatefulWidget {
+  const TimForm({super.key});
+
+  @override
+  State<TimForm> createState() => _TimFormState();
+}
+
+class _TimFormState extends State<TimForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
