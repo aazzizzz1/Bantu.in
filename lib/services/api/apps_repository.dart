06e9c03cbbad9f0593a5d4d563ff16f4b650d0c8
aazzizzz1 <azizvbi33@gmail.com>
2023-05-24 09:25:01 +0700

    merger

--- lib/services/api/apps_repository.dart
+++ lib/services/api/apps_repository.dart
@@ -5,6 +5,7 @@ import 'package:bantuin/models/note_model.dart';
 import 'package:bantuin/models/password_model.dart';
 import 'package:bantuin/models/post_password_model.dart';
 import 'package:bantuin/models/post_user_model.dart';
+import 'package:bantuin/models/tim_model.dart';
 import 'package:bantuin/models/user_models.dart';
 import 'package:bantuin/services/api/api_services.dart';
 import '../../models/login_model.dart';
@@ -109,6 +110,106 @@ class AppsRepository {
           .where((element) =>
               element.status.toLowerCase().contains(value.toLowerCase()))
           .toList();
+    } catch (_) {
+      rethrow;
+    }
+  }
+Future<void> postFileClient(
+      {required String id, required List<File> selectedFile}) async {
+    try {
+      await _apiService.postMultipart(
+          '/attaches', selectedFile, {'note_id': id}, 'path[]');
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+//profile 
+// profile user
+  Future getUsers() async {
+    try {
+      final response = await _apiService.getRequest('/active_user');
+      PasswordModel users = PasswordModel.fromJson(response['data']);
+      return users;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> postUsers(PostUsersModel user) async {
+    try {
+      await _apiService.postRequest('/users', user);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> updateUsers(PostUsersModel user, int id) async {
+    try {
+      await _apiService.putRequest('/users/$id', user);
+    } catch (_) {
+      rethrow;
+    }
+  }
+  
+  // FEATURE TEAM
+  Future<void> postAddTim(PostTimModel tim) async {
+    try {
+      final response = await _apiService.postRequest('/teams', tim);
+      print(response);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future getAllTim() async {
+    try {
+      final response = await _apiService.getRequest('/teams');
+      TeamModel team = TeamModel.fromJson(response);
+      return team.team;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future updateTimName(int id, PostTimModel data) async {
+    try {
+      await _apiService.putRequest('/teams/$id', data);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  //passowrd
+  Future getPassword() async {
+    try {
+      final response = await _apiService.getRequest('/update_password');
+      PasswordModel pass = PasswordModel.fromJson(response['data']);
+      return pass;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> postPassword(PostPasswordModel passpost) async {
+    try {
+      await _apiService.postRequest('/update_password', passpost);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> updatePassword(PostPasswordModel passpost, int id) async {
+    try {
+      await _apiService.putRequest('/update_password/$id', passpost);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> postProfilePhoto(UsersDetailModel user, int id) async {
+    try {
+      await _apiService.postRequest('/users/$id', user);
     } catch (_) {
       rethrow;
     }
